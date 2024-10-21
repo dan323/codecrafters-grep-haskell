b1889c39d82ef384f8dddaa408f1335b1462c3ab
Daniel de la Concepción <d.concepcion@ocado.com>
2024-10-22 00:33:43 +0200

    fix: Neg instead of Disj

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,13 +1,15 @@
 module Parser (match, PatternParser(..), Pattern(..), patternParser) where
 
-import Text.Megaparsec (Parsec, many, noneOf, (<?>), choice)
+import Text.Megaparsec (Parsec, many, noneOf, (<?>), choice, try)
 import Text.Megaparsec.Char (string, char)
 import Data.Void (Void)
 import Data.Functor (($>))
 import Control.Applicative ((<|>))
 import Data.Char (isDigit)
+import GHC.IO.Handle (NewlineMode(inputNL))
 
 data Pattern = Digit | AlphaNum | Disj [Pattern] | Char Char | Neg [Pattern]
+    deriving Show
 
 type Parser = Parsec Void String
 
@@ -23,13 +25,13 @@ charParser :: PatternParser
 charParser = (Char <$> noneOf "[]^\\") <?> "singleChar"
 
 patternParser :: PatternParser
-patternParser = choice [digitParser, alphaNumParser, disjointParser, charParser]
+patternParser = choice [digitParser, alphaNumParser, try negativeParser, disjointParser, charParser]
 
 disjointParser :: PatternParser
 disjointParser = (Disj <$> ((char '[' *> many patternParser) <* char ']')) <?> "choice"
 
 negativeParser :: PatternParser
-negativeParser = (Disj <$> ((char '[' *> char '^' *> many patternParser) <* char ']')) <?> "choice"
+negativeParser = (Neg <$> ((char '[' *> char '^' *> many patternParser) <* char ']')) <?> "choice"
 
 match:: Pattern -> String -> Bool
 match Digit input = any isDigit input
@@ -38,9 +40,7 @@ match (Char c) input = c `elem` input
 match (Disj []) _ = False
 match (Disj [p]) input = match p input
 match (Disj (p:ps)) input = match p input || match (Disj ps) input
-match (Neg []) _ = True
-match (Neg [p]) input = not $ match p input
-match (Neg (p:ps)) input = not (match p input) && not (match (Disj ps) input)
+match (Neg ps) input = not $ match (Disj ps) input
 
 isAlphaNum :: Char -> Bool
 isAlphaNum c = ((c <= 'z') && (c >= 'a')) || isDigit c || ((c <= 'Z') && (c >= 'A')) || (c == '_')
