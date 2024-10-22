2a6351db3bdb3a7721f9d676a6f7510e05776b1c
Daniel de la Concepción <d.concepcion@ocado.com>
2024-10-22 02:21:23 +0200

    Try converting patter to parser

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,25 @@ module Main where
 import Data.Char (isDigit)
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
-import Parser as P (patternParser, match)
+import Parser as P (completePatterParser, match)
 import Text.Megaparsec (parse,errorBundlePretty, match)
+import Data.Bool (bool)
 
 main :: IO ()
 main = do
   args <- getArgs
   let pattern = args !! 1
   input <- getLine
-  let patt = case parse P.patternParser "" pattern of
+  let patt = case parse P.completePatterParser "" pattern of
               Left x -> error $ errorBundlePretty x
               Right y -> y
 
+  print patt
   if head args /= "-E"
     then do
       putStrLn "Expected first argument to be '-E'"
       exitFailure
     else do
-      if P.match patt input
-        then exitSuccess
-        else exitFailure
+      either (const exitFailure) 
+             (bool exitFailure exitSuccess) 
+                $ parse (P.match patt) "" input
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,19 +1,21 @@
-module Parser (match, PatternParser(..), Pattern(..), patternParser) where
+module Parser (match, PatternParser(..), Pattern(..), completePatterParser) where
 
-import Text.Megaparsec (Parsec, many, noneOf, (<?>), choice, try)
+import Text.Megaparsec (Parsec, many, noneOf, (<?>), choice, try, satisfy, anySingle, token, MonadParsec (eof))
 import Text.Megaparsec.Char (string, char)
 import Data.Void (Void)
 import Data.Functor (($>))
 import Control.Applicative ((<|>))
 import Data.Char (isDigit)
-import GHC.IO.Handle (NewlineMode(inputNL))
+import Control.Applicative.Combinators (option)
+import qualified Data.Set as Set (empty)
 
-data Pattern = Digit | AlphaNum | Disj [Pattern] | Char Char | Neg [Pattern]
+data Pattern = Digit | AlphaNum | Disj [Pattern] | Char Char | Neg [Pattern] | Seq [Pattern]
     deriving Show
 
 type Parser = Parsec Void String
 
 type PatternParser = Parser Pattern
+type Matcher = Pattern -> Parser Bool
 
 digitParser :: PatternParser
 digitParser = (string "\\d" $> Digit) <?> "digit"
@@ -24,23 +26,28 @@ alphaNumParser = (string "\\w" $> AlphaNum) <?> "alphaNum"
 charParser :: PatternParser
 charParser = (Char <$> noneOf "[]^\\") <?> "singleChar"
 
+completePatterParser :: PatternParser
+completePatterParser = Seq <$> (many patternParser <* eof)
+
 patternParser :: PatternParser
-patternParser = choice [digitParser, alphaNumParser, try negativeParser, disjointParser, charParser]
+patternParser = choice . fmap try $ [digitParser, alphaNumParser, negativeParser, disjointParser, charParser]
 
 disjointParser :: PatternParser
 disjointParser = (Disj <$> ((char '[' *> many patternParser) <* char ']')) <?> "choice"
 
 negativeParser :: PatternParser
-negativeParser = (Neg <$> ((char '[' *> char '^' *> many patternParser) <* char ']')) <?> "choice"
+negativeParser = (Neg <$> ((char '[' *> char '^' *> many patternParser) <* char ']')) <?> "negativeGroup"
 
-match:: Pattern -> String -> Bool
-match Digit input = any isDigit input
-match AlphaNum input = any isAlphaNum input
-match (Char c) input = c `elem` input
-match (Disj []) _ = False
-match (Disj [p]) input = match p input
-match (Disj (p:ps)) input = match p input || match (Disj ps) input
-match (Neg ps) input = not $ match (Disj ps) input
+match:: Matcher
+match Digit = (option False $ satisfy isDigit $> True) <?> "digit"
+match AlphaNum = (option False $ satisfy isAlphaNum $> True) <?> "alphaNum"
+match (Char c) = (option False $ satisfy (==c) $> True) <?> "char"
+match (Disj []) = token (const $ Just False) Set.empty <?> "disjEmpty"
+match (Disj (p:ps)) = match p <|> match (Disj ps) <?> "disj"
+match (Neg ps) = not <$> match (Disj ps) <?> "negGroup"
+match (Seq []) = token (const $ Just False) Set.empty <?> "seqEmpty"
+match (Seq [p]) = match p <?> "single p"
+match (Seq (p:ps)) = (match p *> match (Seq ps)) <?> "seq"
 
 isAlphaNum :: Char -> Bool
 isAlphaNum c = ((c <= 'z') && (c >= 'a')) || isDigit c || ((c <= 'Z') && (c >= 'A')) || (c == '_')
