ee9d3e146a795ba235b7b3bdb8d4cc596d2ecdc1
dnl232 <danconsa@hotmail.com>
2024-10-25 02:31:13 +0200

    propper errors

--- app/Parser.hs
+++ app/Parser.hs
@@ -12,7 +12,7 @@ import Data.List as L (singleton)
 import Data.Set qualified as Set (empty)
 import Data.Text as T (Text, take, unpack)
 import Data.Void (Void)
-import Text.Megaparsec (MonadParsec (eof), Parsec, anySingle, choice, getParserState, many, manyTill, noneOf, oneOf, satisfy, stateInput, stateOffset, token, try, (<?>))
+import Text.Megaparsec (MonadParsec (eof), Parsec, anySingle, choice, getParserState, many, manyTill, noneOf, oneOf, satisfy, stateInput, stateOffset, token, try, (<?>), parseError, ParseError(..))
 import Text.Megaparsec.Char (char, string)
 import Text.Megaparsec.Debug (dbg')
 
@@ -124,26 +124,33 @@ negativeParser = Neg <$> (string "[^" *> manyTill (charParser >>= returnChar) (c
 match :: Matcher
 match = matchWithGroups []
   where
-    matchWithGroups _ [] = pure ()
-    matchWithGroups gs (Digit : ps) = satisfy isDigit *> matchWithGroups gs ps
+    matchWithGroups _  [] = pure ()
+    matchWithGroups gs (Digit : ps) =  satisfy isDigit *> matchWithGroups gs ps
     matchWithGroups gs (AlphaNum : ps) = satisfy isAlphaNum *> matchWithGroups gs ps
-    matchWithGroups gs (Char c : ps) = dbg' ("charDebug[" ++ [c] ++ "]") (char c) *> matchWithGroups gs ps
-    matchWithGroups _ (Pos [] : _) = error "this cannot be"
-    matchWithGroups gs (Alt p1 p2 : xs) = try (matchWithGroups gs (p1 ++ xs)) <|> dbg' "altDebug2" (matchWithGroups gs (p2 ++ xs))
-    matchWithGroups gs (Pos (c : cs) : xs) = dbg' "posDebug" (matchWithGroups gs (Alt [Char c] [Pos cs] : xs))
+    matchWithGroups gs (Char c : ps) = char c *> matchWithGroups gs ps
+    matchWithGroups _  (Pos [] : _) = do
+        state <- getParserState
+        let processed = stateOffset state
+        parseError $ FancyError processed Set.empty
+    matchWithGroups gs (Alt p1 p2 : xs) = try (matchWithGroups gs (p1 ++ xs)) <|> matchWithGroups gs (p2 ++ xs)
+    matchWithGroups gs (Pos (c : cs) : xs) = matchWithGroups gs (Alt [Char c] [Pos cs] : xs)
     matchWithGroups gs (Neg [] : xs) = anySingle $> ()
-    matchWithGroups gs (Neg (p : ps) : xs) = try (char p *> error "This is an error") <|> matchWithGroups gs (Neg ps : xs)
+    matchWithGroups gs (Neg (p : ps) : xs) = try (char p *> (do
+        state <- getParserState
+        let processed = stateOffset state
+        parseError $ FancyError processed Set.empty
+        )) <|> matchWithGroups gs (Neg ps : xs)
     matchWithGroups gs (((Optional p) : ps)) = try (matchWithGroups gs (p : ps)) <|> matchWithGroups gs ps
-    matchWithGroups gs (((ZeroOrMore p) : ps)) = try (dbg' "zeroOrMoreFinishedDbg" (matchWithGroups gs ps)) <|> dbg' "zeroOrMoreDgb" (matchWithGroups gs (p : ZeroOrMore p : ps))
+    matchWithGroups gs (((ZeroOrMore p) : ps)) = try (matchWithGroups gs (p: ZeroOrMore p : ps)) <|> matchWithGroups gs ps
     matchWithGroups gs (((OneOrMore p) : ps)) = matchWithGroups gs (p : ZeroOrMore p : ps)
     matchWithGroups _ (Start : ps) = do
       state <- getParserState
       let processed = stateOffset state
       if processed == 0
         then pure ()
-        else error "No match"
+        else error "Invalid Pattern"
     matchWithGroups _ [End] = eof
-    matchWithGroups _ (End : xs) = error "Expected end of input"
+    matchWithGroups _ (End : xs) = error "Invalid Pattern"
     matchWithGroups gs (Wildcard : xs) = anySingle *> matchWithGroups gs xs
     matchWithGroups gs (Group ps : xs) = dbg' "groupDbg" $ do
       statusA <- getParserState
@@ -155,7 +162,7 @@ match = matchWithGroups []
       let consumed = T.take (end - initial) input
       let patt = Char <$> T.unpack consumed
       matchWithGroups (gs ++ [patt]) xs
-    matchWithGroups gs (BackRef x : ps) = dbg' "groupDbg" $ matchWithGroups gs ((gs !! (x - 1)) ++ ps)
+    matchWithGroups gs (BackRef x : ps) = matchWithGroups gs ((gs !! (x - 1)) ++ ps)
 
 partialMatch :: Matcher -> Matcher
 partialMatch match p = skipManyTill anySingle (try $ match p) <* many anySingle <* eof
